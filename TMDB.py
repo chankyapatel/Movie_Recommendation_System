778048525e2c1ad7a2c957e299b41c4555466462
chankyapatel <chanakyapatel619@gamil.com>
2022-07-18 21:02:28 -0400

    Updated script

--- TMDB.py
+++ TMDB.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# # Libraries
+# # Recommendation System
+
+# ### Libraries
 
 # #### - request allows us to send HTTP requests using python. The HTTPS requests returns a response object like data.
 # #### - The cosine similarity is used for matching similar documents based on maximum number of common words in documents.
@@ -18,6 +20,8 @@ from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import linear_kernel
 from sklearn.feature_extraction.text import TfidfVectorizer
+import warnings
+warnings.filterwarnings('ignore')
 
 
 # In[2]:
@@ -29,6 +33,7 @@ import json
 # In[3]:
 
 
+# To get the API
 response = requests.get("https://api.themoviedb.org/3/movie/550?api_key=82860678603c30cb74ba5f32be02e2f7")
 
 
@@ -39,51 +44,72 @@ response = requests.get("https://api.themoviedb.org/3/movie/550?api_key=82860678
 # In[4]:
 
 
+# Checking the data
 print(response.json())
 
 
-# ### For Loop
+# ### For Loop & Creating Json file
 
 # In[5]:
 
 
-movie_id = list(range(11, 950))
+# This loop will fatch more movies and also it will convert them into Json format
+movie_id = list(range(11, 950)) # Here took 11 beacause 0-10 has not data of movie.
 
 for i in range(len(movie_id)):
     url = 'https://api.themoviedb.org/3/movie/{}?api_key=82860678603c30cb74ba5f32be02e2f7'.format(movie_id[i])
     response = requests.get(url).json()
     text = json.dumps(response, sort_keys = True, indent = 4)
+     #sort_keys = True tells the encoder to return the JSON object keys in a sorted order
     print(text)
-    with open('TMDB.json', 'w') as json_file:
+    with open('TMDB.json', 'w') as json_file:  # This is where it is converting it into json file.
         json.dump(response, json_file)
 
 
 # ## After getting all the data i have converted it into csv file.
 
-# In[6]:
+# In[3]:
 
 
 Movies_df = pd.read_csv("C:\\Users\\Patel Chankya\\Desktop\\Capstone Project\\TMDB.csv")
 
 
-# In[7]:
+# In[4]:
 
 
 Movies_df
 
 
+# In[5]:
+
+
+Movies_df.columns
+
+
+# In[6]:
+
+
+Movies_df.shape
+
+
 # ## Dropping unnecessary features
 
-# In[8]:
+# In[7]:
 
 
 Movies_df.drop(labels = ['belongs_to_collection.name','adult','backdrop_path','belongs_to_collection.backdrop_path','belongs_to_collection.id','belongs_to_collection.poster_path','imdb_id','poster_path','status','tagline','video','status_code','status_message','success'], axis =1,inplace = True)
 
 
-# In[9]:
+# In[8]:
 
 
 Movies_df
+
+
+# In[9]:
+
+
+Movies_df = Movies_df.rename({'vote_average': 'Ratings'}, axis=1)
 
 
 # ## Features
@@ -105,80 +131,65 @@ Movies_df.isnull().sum()
 # In[12]:
 
 
-Movies_df.describe()
+Nullvalues = Movies_df.isnull().sum()
+Nullvalues = Nullvalues.to_csv("Nullvalues.csv",index = True)
 
 
-# ## Rounding all the values
+# ## Dropping the blank rows from the dataset
 
 # In[13]:
 
 
-Movies_df.describe().round(0)
+Movies_df = Movies_df.dropna(how = 'all')
 
-
-# # Replacing Null Values
-
-# #### - Replacing all the null values with KNN(K-Nearest Neighbors)
 
 # In[14]:
 
 
-from sklearn.impute import KNNImputer
-
-
-# In[15]:
-
-
-import numpy as np
-
-
-# In[16]:
-
-
-imputer = KNNImputer(n_neighbors=5)
-
-
-# In[17]:
-
-
-movies = ["vote_average", "vote_count","popularity","budget","revenue"]
+Movies_df.isnull().sum()
 
 
 # In[18]:
 
 
-Movies_df[movies].isnull().sum()
+Movies_df.shape
 
 
-# In[19]:
+# In[15]:
 
 
-A = Movies_df[movies]
+Movies_df.describe()
 
 
-# In[20]:
+# In[16]:
 
 
-Movies = pd.DataFrame(imputer.fit_transform(A),columns = A.columns)
+describe = Movies_df.describe()
 
 
-# In[21]:
+# In[18]:
 
 
-Movies.isnull().sum()
+describe.to_csv("describe.csv",index = True)
 
 
-# #### - Droppping all the unnecessary null values from DataSet
+# ## Rounding all the values
 
-# In[22]:
+# In[17]:
 
 
-Movies_df = Movies_df.dropna(subset=['genres','id','original_language','original_title','overview','production_companies','production_countries','release_date','runtime','spoken_languages'])
+Movies_df.describe().round(0)
+
+
+# In[18]:
+
+
+Movies_df
 
 
 # # Data Types
 
-# In[23]:
+# In[19]:
 
 
 Movies_df.dtypes
@@ -186,21 +197,53 @@ Movies_df.dtypes
 
 # #### - Changing the release_date dtype to datetime
 
-# In[24]:
+# In[20]:
 
 
 Movies_df['release_date'] = pd.to_datetime(Movies_df['release_date'])
 
 
-# In[25]:
+# In[21]:
 
 
 Movies_df.dtypes
 
 
-# # Initial Dataset
+# ## Unique values
+
+# In[22]:
+
+
+import itertools
+
+
+# In[23]:
+
+
+import numpy as np
+
+
+# In[24]:
+
+
+np.unique([*itertools.chain.from_iterable(Movies_df.genres)])
+
+
+# In[25]:
+
+
+np.unique([*itertools.chain.from_iterable(Movies_df.spoken_languages)])
+
 
 # In[26]:
+
+
+np.unique([*itertools.chain.from_iterable(Movies_df.production_countries)])
+
+
+# # Initial Dataset
+
+# In[27]:
 
 
 Movies_df1 = Movies_df[['genres','production_countries','spoken_languages']]
@@ -209,15 +252,16 @@ Movies_df1
 
 # ### Using Abstract Syntax Grammer
 
-# In[27]:
+# In[28]:
 
 
+# AST it is a tree representation of the abstract syantatic structure of text written in a formal language.
 import ast 
 
 
 # #### - Defining get_dict_val containing a for loop which will return name.
 
-# In[28]:
+# In[29]:
 
 
 def get_dict_val(data):
@@ -232,37 +276,38 @@ def get_dict_val(data):
 
 # #### - Applying get_dict_val in each column.
 
-# In[29]:
+# In[30]:
 
 
 Movies_df['genres']=Movies_df['genres'].apply(get_dict_val)
 
 
-# In[30]:
-
-
-Movies_df['genres'][6]
-
-
 # In[31]:
 
 
-Movies_df['production_countries']=Movies_df['production_countries'].apply(get_dict_val)
+# Here [6] is the 6th movie number
+Movies_df['genres'][6]
 
 
 # In[32]:
 
 
-Movies_df['production_countries']
+Movies_df['production_countries']=Movies_df['production_countries'].apply(get_dict_val)
 
 
 # In[33]:
 
 
-Movies_df['spoken_languages']=Movies_df['spoken_languages'].apply(get_dict_val)
+Movies_df['production_countries']
 
 
 # In[34]:
+
+
+Movies_df['spoken_languages']=Movies_df['spoken_languages'].apply(get_dict_val)
+
+
+# In[35]:
 
 
 Movies_df['spoken_languages']
@@ -270,83 +315,131 @@ Movies_df['spoken_languages']
 
 # # Dataset after applying AST
 
-# In[35]:
+# In[36]:
 
 
 Movies_df
 
 
-# # Top Trending Movies
-
-# In[36]:
-
-
-import matplotlib.pyplot as plt
-
+# ## Unique values after cleaning the data
 
 # In[37]:
 
 
-# Plot top 10 movies
-# for every user
-def plot():
-    popularity = Movies_df.sort_values("popularity", ascending=False)
-    plt.figure(figsize=(12, 6))
-    plt.barh(popularity["title"].head(10), popularity["popularity"].head(10), align="center", color="green")
-    plt.gca().invert_yaxis()
-    plt.title("Top Trending movies")
-    plt.xlabel("Popularity")
-    plt.show()
-    
+np.unique([*itertools.chain.from_iterable(Movies_df.genres)])
 
-plot()
-
-
-# # Weighted Ratings
-
-# #### - Creating 2 vectors one contains mean of "Vote_average" and second contain at 0.9 quantile of "Vote_count"
 
 # In[38]:
 
 
-C = Movies_df["vote_average"].mean()
-m = Movies_df["vote_count"].quantile(0.9)
+np.unique([*itertools.chain.from_iterable(Movies_df.spoken_languages)])
 
-print("C: ", C)
-print("m: ", m)
-
-new_Movies_df = Movies_df.copy().loc[Movies_df["vote_count"] >= m]
-print(new_Movies_df.shape)
-
-
-# #### - Weighted ratings works on a mathematical formula
 
 # In[39]:
 
 
-def weighted_rating(x, C=C, m=m):
-    v = x["vote_count"]
-    R = x["vote_average"]
-
-    return (v/(v + m) * R) + (m/(v + m) * C)
+np.unique([*itertools.chain.from_iterable(Movies_df.production_countries)])
 
 
-# # Ratings
+# # Top Trending Movies
 
-# #### - Top 10 Movies based on Highest ratings
+# In[43]:
+
+
+get_ipython().system(' pip install powerbiclient')
+
 
 # In[40]:
 
 
-new_Movies_df["Ratings"] = new_Movies_df.apply(weighted_rating, axis=1)
-new_Movies_df = new_Movies_df.sort_values('Ratings', ascending=False)
+from powerbiclient import Report, models
 
-new_Movies_df[["original_title", "vote_count", "vote_average", "Ratings"]].head(10)
+
+# In[41]:
+
+
+from powerbiclient.authentication import DeviceCodeLoginAuthentication
+
+
+# In[42]:
+
+
+device_auth = DeviceCodeLoginAuthentication()
+
+
+# In[43]:
+
+
+group_id="50903f3f-667f-4268-abb7-82d4f439d40f"
+report_id="5788edf0-013c-4df5-80b5-d613292a34f3"
+
+
+# In[44]:
+
+
+report = Report(group_id=group_id, report_id=report_id, auth=device_auth)
+
+report
+
+
+# # Ratings
+
+# In[49]:
+
+
+minvalue = Movies_df['Ratings'].min()
+
+
+# In[50]:
+
+
+minvalue
+
+
+# In[51]:
+
+
+maxvalue = Movies_df['Ratings'].max()
+
+
+# In[52]:
+
+
+maxvalue
+
+
+# In[53]:
+
+
+Movies_df.loc[Movies_df['title'] == "Scarface"]
+
+
+# In[45]:
+
+
+device_auth = DeviceCodeLoginAuthentication()
+
+
+# In[46]:
+
+
+group_id="390c6246-85e7-4101-8a19-b82b5b6ea0e9"
+report_id="c21ed7bd-d825-4174-9b3d-d54465155931"
+
+
+# #### - Top 10 Movies based on Highest ratings
+
+# In[47]:
+
+
+report1 = Report(group_id=group_id, report_id=report_id, auth=device_auth)
+
+report1
 
 
 # # Features
 
-# In[41]:
+# In[48]:
 
 
 features = ['spoken_languages','genres','overview','production_countries']
@@ -354,7 +447,7 @@ features = ['spoken_languages','genres','overview','production_countries']
 
 # #### - Now lets convert the above features into lowercase and remove all the spaces between them
 
-# In[42]:
+# In[49]:
 
 
 # Creating clean_data which contains if else loop to lower the case and remove the spaces between them.
@@ -368,7 +461,7 @@ def clean_data(x):
             return ""
 
 
-# In[43]:
+# In[50]:
 
 
 for feature in features:
@@ -377,7 +470,7 @@ for feature in features:
 
 # # Soup
 
-# In[44]:
+# In[51]:
 
 
 # create_soup joins all the features.
@@ -389,25 +482,31 @@ Movies_df["soup"] = Movies_df.apply(create_soup, axis=1)
 print(Movies_df["soup"].head())
 
 
-# ### - Now we will create a reverse mapping of title to indices. So that we can easily find the title based on the index
+# # indexing - it returns the index where the element appears in Data Frame
 
-# In[49]:
+# In[52]:
 
 
-# index - it returns the index where the element appears in Data Frame
 indices = pd.Series(Movies_df.index, index=Movies_df["title"]).drop_duplicates()
 print(indices.head())
+
+
+# In[62]:
+
+
+indices_2 = pd.Series(Movies_df.index, index=Movies_df["genres"]).drop_duplicates()
+print(indices_2.head())
 
 
 # # CountVectorizer
 
 # #### - Creating tokens
-# #### - Creating cosine_sin
+# #### - Creating cosine_sim
 
-# In[50]:
+# In[53]:
 
 
-# stop words can be remove from data because they dont add meaning to a sentence.
+# stop words can be remove from data because they dont add any meaning to a sentence.
 count_vectorizer = CountVectorizer(stop_words="english")
 count_matrix = count_vectorizer.fit_transform(Movies_df["soup"])
 
@@ -417,46 +516,48 @@ print(count_matrix.shape)
 cosine_sim = cosine_similarity(count_matrix, count_matrix)
 print(cosine_sim.shape)
 
-movies_df = Movies_df.reset_index()
+movies_df = Movies_df.reset_index() # it will reset the index
 indices = pd.Series(movies_df.index, index=movies_df['title'])
 
 
-# # Creating Recommendation
+# # Creating Recommendation Function
 
-# In[57]:
+# In[54]:
 
 
-# we will create a function named get_recommendations, which will take title and cosine similarity score as an input.
+# we will create a function named get_recommendations, which contains title and cosine similarity.
 def get_recommendations(title, cosine_sim=cosine_sim):
-    """
-    in this function,
-        we take the cosine score of given movie
-        sort them based on cosine score (movie_id, cosine_score)
-        take the next 10 values because the first entry is itself
-        get those movie indices
-        map those indices to titles
-        return title list
-    """
     idx = indices[title]
+    
 # This sim_scores will make tuples where the first element is index and second element as cosine similarity score.
     sim_scores = list(enumerate(cosine_sim[idx]))
+    
 # This sim_score will sort the list of tuples in descending order based on the similarity score.
 # lamda - lamda will create an inline function.
 # x - the inline function will take x as an input and it willl return x[1].
-# x[1] - it is the second elemnt of x.
+# x[1] - it is the second element of x.
     sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
+    
 # why 1? because indexing starts from 0 and it is the title of the movie itself.
     sim_scores = sim_scores[1:11] 
+    
 # Now we will map indices to the title.
     movies_indices = [ind[0] for ind in sim_scores]
     movies = Movies_df["title"].iloc[movies_indices]
     return movies # It will return the movie list
 
 
-# In[91]:
+# # Recommendation
+
+# In[57]:
 
 
-print("Finding Nemo")
-print(get_recommendations("Finding Nemo"))
-print()
+name = str(input("Please enter the name of the movie: "))
+
+
+# In[58]:
+
+
+print(name)
+print(get_recommendations(name))
 
